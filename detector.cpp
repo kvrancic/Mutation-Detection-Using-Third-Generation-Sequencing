5116d0607db193da3f72332729f51d79962ebbc8
Tea Tisaj <tea.c.tisaj@gmail.com>
2024-05-15 00:37:52 +0200

    proba

--- detector.cpp
+++ detector.cpp
@@ -8,12 +8,16 @@
 
 using namespace std;
 
-// Structure to represent a mutation
-//struct Mutation {
-//    string Type;           // Type of mutation (Substitution, Insertion, Deletion, etc.)
-//    int Position;          // Position of the mutation
-//    string Base;           // Base or sequence involved in the mutation
-// };
+// Structure to store mutation information
+struct Mutation {
+    char type;
+    int position;
+    char nucleotide;
+
+    Mutation(char t, int pos, char nuc) : type(t), position(pos), nucleotide(nuc) {}
+};
+
+
 
 
 // Function to execute system command
@@ -23,6 +27,99 @@ void executeCommand(const string& command) {
         cerr << "Command failed: " << command << endl;
         exit(EXIT_FAILURE);
     }
+}
+
+
+// Function to parse the CIGAR string and identify mutations
+void parseCigarString(const string& cigar, int referenceStart, vector<Mutation>& mutations) {
+    int position = referenceStart; // Initialize position on the reference genome
+
+    // Parse the CIGAR string
+    size_t pos = 0;
+    while (pos < cigar.length()) {
+        // Extract the length of each operation
+        size_t end = cigar.find_first_of("MIDNSHP=X", pos);
+        if (end == string::npos) {
+            end = cigar.length();
+        }
+        int length = stoi(cigar.substr(pos, end - pos));
+
+        // Determine the operation
+        char operation = cigar[end];
+        if (operation == 'X') {
+            // Substitution mutation
+            for (int i = 0; i < length; ++i) {
+                int poz;
+                char nucleotide; // Fetch the nucleotide from the read (for example)
+                // Store the mutation information
+                poz = position + i;
+                mutations.push_back(Mutation('X', poz, nucleotide));
+            }
+        } else if (operation == 'I') {
+            // Insertion mutation
+            char nucleotide; // Fetch the inserted nucleotide from the read (for example)
+            // Store the mutation information
+            mutations.push_back(Mutation('I', position - 1, nucleotide)); // Position before insertion
+        } else if (operation == 'D') {
+            // Deletion mutation
+            // Store the mutation information
+            mutations.push_back(Mutation('D', position, '-'));
+            // Move the reference position forward by the length of the deletion
+            position += length;
+        } else if (operation == 'M') {
+            // Match operation, move the reference position forward
+            position += length;
+        }
+
+        // Move to the next operation in the CIGAR string
+        pos = end + 1;
+    }
+}
+
+// Updated function to identify mutations from the PAF file
+vector<Mutation> identifyMutationsFromPAF(const string& pafFilename) {
+    vector<Mutation> mutations;
+
+    ifstream file(pafFilename);
+    if (!file.is_open()) {
+        cerr << "Unable to open file " << pafFilename << endl;
+        return mutations;
+    }
+
+    // Iterating through each line of the PAF file
+    string line;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        string cigar;
+        int referenceStart, referenceEnd;
+        // Reading necessary data from the PAF line
+        iss >> cigar >> referenceStart >> referenceEnd;
+        // Parse the CIGAR string and identify mutations
+        parseCigarString(cigar, referenceStart, mutations);
+    }
+
+    file.close();
+    return mutations;
+}
+
+
+// Function to write mutations to a CSV file
+void writeMutationsToCSV(const vector<Mutation>& mutations, const string& filename) {
+    ofstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Unable to open file " << filename << endl;
+        return;
+    }
+
+    // Writing CSV file header
+    file << "Mutation,Position,Nucleotide" << endl;
+
+    // Writing each mutation in CSV format
+    for (const auto& mutation : mutations) {
+        file << mutation.type << "," << mutation.position << "," << mutation.nucleotide << endl;
+    }
+
+    file.close();
 }
 
 // Function to parse the SAM file and extract mutations
@@ -99,39 +196,13 @@ void detectMutations(const string& samFile, const string& outputFile) {
     outFile.close();
 }
 
-// Function to load variants from the VCF file generated from FreeBayes
-// vector<Mutation> loadVariantsFromVCF(const string& vcfFile) {
-//     ifstream inFile(vcfFile);
-//     vector<Mutation> variants;
-//     string line;
-
-//     while (getline(inFile, line)) {
-//         if (line[0] == '#') continue; // Skip header lines
-
-//         istringstream iss(line);
-//         string chr, pos, id, ref, alt, qual, filter, info, format, sample;
-
-//         // Extract fields from the VCF line
-//         iss >> chr >> pos >> id >> ref >> alt >> qual >> filter >> info >> format >> sample;
-
-//         if (alt != ".") { // If variant already exists
-//             // Take only the first variant
-//             istringstream altStream(alt);
-//             string firstAlt;
-//             getline(altStream, firstAlt, ',');
-//             variants.push_back({"Variant", stoi(pos) - 1, firstAlt}); // -1 because of zero-based position
-//         }
-//     }
-
-//     return variants;
-// }
-
 
 int main() {
     // This is hard-coded for simplicity, but could be passed as command-line arguments
     string reference = "data/lambda.fasta";
     string reads = "data/lambda_simulated_reads.fasta";
     string samOutput = "data/alignment.sam";
+    string pafOutput = "data/aligment2.paf";
     string bamOutput = "data/alignment.bam";
     string sortedBam = "data/alignment_sorted.bam";
     string csvOutput = "data/mutations.csv";
@@ -143,49 +214,57 @@ int main() {
 
 
     // Step 1: Run Minimap2
-    string minimapCmd = "minimap2 -ax map-ont " + reference + " " + reads + " > " + samOutput;
+    string minimapCmd = "minimap2 --eqx -c -ax map-ont " + reference + " " + reads + " > " + pafOutput;
     cout << "Running Minimap2: " << minimapCmd << endl;
     executeCommand(minimapCmd);
 
-    // Step 2: Parse SAM file to detect mutations
-    detectMutations(samOutput, csvOutput);
 
-    cout << "Mutation detection completed. Results are stored in " << csvOutput << endl;
+    // Step 2: Identify mutations from the PAF file
+    vector<Mutation> mutations = identifyMutationsFromPAF(pafOutput);
+    // Step 3: Write mutations to a CSV file
+    writeMutationsToCSV(mutations, csvOutput);
 
-    // Step 3: Generate index for the reference genome
-    cout << "Generating index for the reference genome..." << endl;
-    executeCommand(indexCmd); // Execute the command to generate index
+    cout << "Mutations are written to " << csvOutput << endl;
 
-    // Step 4: Convert SAM to BAM
-    string samToBamCmd = "samtools view -bS " + samOutput + " > " + bamOutput;
-    cout << "Converting SAM to BAM: " << samToBamCmd << endl;
-    executeCommand(samToBamCmd);
+    // // Step 2: Parse SAM file to detect mutations
+    // detectMutations(samOutput, csvOutput);
 
-    // Step 5: Sort BAM file
-    string sortCmd = "samtools sort -o " + sortedBam + " " + bamOutput;
-    cout << "Sorting BAM file: " << sortCmd << endl;
-    executeCommand(sortCmd);
+    // cout << "Mutation detection completed. Results are stored in " << csvOutput << endl;
 
-    // Step 6: Mark duplicates‚
-    string markDuplicatesCmd = "picard MarkDuplicates I=" + sortedBam + " O=" + markedBam + " M=" + metricsFile + " REMOVE_DUPLICATES=true";
-    cout << "Marking duplicates: " << markDuplicatesCmd << endl;
-    executeCommand(markDuplicatesCmd);
+    // // Step 3: Generate index for the reference genome
+    // cout << "Generating index for the reference genome..." << endl;
+    // executeCommand(indexCmd); // Execute the command to generate index
 
-    // Step 7: Index marked BAM file
-    string indexMarkedCmd = "samtools index " + markedBam;
-    cout << "Indexing marked BAM file: " << indexMarkedCmd << endl;
-    executeCommand(indexMarkedCmd);
+    // // Step 4: Convert SAM to BAM
+    // string samToBamCmd = "samtools view -bS " + samOutput + " > " + bamOutput;
+    // cout << "Converting SAM to BAM: " << samToBamCmd << endl;
+    // executeCommand(samToBamCmd);
+
+    // // Step 5: Sort BAM file
+    // string sortCmd = "samtools sort -o " + sortedBam + " " + bamOutput;
+    // cout << "Sorting BAM file: " << sortCmd << endl;
+    // executeCommand(sortCmd);
+
+    // // Step 6: Mark duplicates‚
+    // string markDuplicatesCmd = "picard MarkDuplicates I=" + sortedBam + " O=" + markedBam + " M=" + metricsFile + " REMOVE_DUPLICATES=true";
+    // cout << "Marking duplicates: " << markDuplicatesCmd << endl;
+    // executeCommand(markDuplicatesCmd);
+
+    // // Step 7: Index marked BAM file
+    // string indexMarkedCmd = "samtools index " + markedBam;
+    // cout << "Indexing marked BAM file: " << indexMarkedCmd << endl;
+    // executeCommand(indexMarkedCmd);
 
 
-    // Step 8: Run FreeBayes for evaluation with optimization
-    string freebayesCmd = "freebayes -f " + reference + " --use-best-n-alleles 4 --min-alternate-count 3 " + markedBam + " > " + vcfOutput;
-    cout << "Running FreeBayes with optimization: " << freebayesCmd << endl;
-    executeCommand(freebayesCmd);
+    // // Step 8: Run FreeBayes for evaluation with optimization
+    // string freebayesCmd = "freebayes -f " + reference + " --use-best-n-alleles 4 --min-alternate-count 3 " + markedBam + " > " + vcfOutput;
+    // cout << "Running FreeBayes with optimization: " << freebayesCmd << endl;
+    // //executeCommand(freebayesCmd);
 
 
 
-    cout << "Variant calling completed. Results are stored in " << vcfOutput << endl;
-     //inspect VCF file: bcftools view data/variants.vcf
+    // cout << "Variant calling completed. Results are stored in " << vcfOutput << endl;
+    //  //inspect VCF file: bcftools view data/variants.vcf
  
     return 0;
 }
