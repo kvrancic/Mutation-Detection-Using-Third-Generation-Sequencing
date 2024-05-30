b35577a12ac28f2da462964c36461892242c0cd8
Tea Tisaj <tea.c.tisaj@gmail.com>
2024-05-30 15:25:39 +0200

    edited MutationProposal elements as vector

--- newest.cpp
+++ newest.cpp
@@ -15,6 +15,23 @@ struct SamEntry {
     string seq;
 };
 
+// Funkcija za ispis vektora char-ova
+ostream& operator<<(ostream& os, const vector<char>& vec) {
+    for (char c : vec) {
+        os << c;
+    }
+    return os;
+}
+
+// Funkcija za ispis vektora stringova
+// ostream& operator<<(ostream& os, const vector<string>& vec) {
+//     for (const string& str : vec) {
+//         os << str << " ";
+//     }
+//     return os;
+// }
+
+
 // Funkcija za parsiranje SAM datoteke
 vector<SamEntry> parseSamFile(const string& filename) {
     vector<SamEntry> entries;
@@ -60,29 +77,28 @@ vector<pair<char, int>> parseCigar(const string& cigar) {
     return operations;
 }
 
-// Struktura za predloge mutacija
 struct MutationProposal {
     int substitutionVotes = 0;
-    char substitutionBase = ' ';
+    vector<char> substitutionBases;
     int insertionVotes = 0;
-    string insertionBases = "";
+    vector<string> insertionBases;
     int deletionVotes = 0;
-    string deletionBases = "";
+    vector<string> deletionBases;
 };
 
 // fja za dodavanje na sve pozicije a ne samo pocetnu
 void addMutationProposal(map<int, MutationProposal>& mutationProposals, int startPos, int count, char mutationType, const string& bases) {
     for (int i = 0; i < count; ++i) {
         int pos = startPos + i;
-        if (mutationType == 'M') { 
+        if (mutationType == 'M') {
             mutationProposals[pos].substitutionVotes++;
-            mutationProposals[pos].substitutionBase = bases[i];
-        } else if (mutationType == 'I') { 
+            mutationProposals[pos].substitutionBases.push_back(bases[i]);
+        } else if (mutationType == 'I') {
             mutationProposals[pos].insertionVotes++;
-            mutationProposals[pos].insertionBases = bases;
-        } else if (mutationType == 'D') { 
+            mutationProposals[pos].insertionBases.push_back(bases.substr(i, 1));
+        } else if (mutationType == 'D') {
             mutationProposals[pos].deletionVotes++;
-            mutationProposals[pos].deletionBases = bases;
+            mutationProposals[pos].deletionBases.push_back(bases.substr(i, 1));
         }
     }
 }
@@ -136,7 +152,7 @@ void detectMutations(const vector<SamEntry>& samEntries, const string& reference
                     refPos += count;
                 }
             } else if (op == 'S') { // Soft clipping
-                readPos += count;
+                                readPos += count;
             } else if (op == 'H') { // Hard clipping
                 continue;
             }
@@ -148,13 +164,62 @@ void detectMutations(const vector<SamEntry>& samEntries, const string& reference
         int pos = proposal.first;
         MutationProposal& votes = proposal.second;
 
-        int maxVotes = max({votes.substitutionVotes, votes.insertionVotes, votes.deletionVotes});
-        if (maxVotes == votes.substitutionVotes && votes.substitutionBase != reference[pos]) {
-            outfile << "substitution,X," << pos << "," << votes.substitutionBase << "\n";
-        } else if (maxVotes == votes.insertionVotes) {
-            outfile << "insertion,I," << pos << "," << votes.insertionBases << "\n";
-        } else if (maxVotes == votes.deletionVotes) {
-            outfile << "deletion,D," << pos << "," << votes.deletionBases << "\n";
+        // Pronalaženje najčešćeg znaka za svaku vrstu mutacije
+        char mostCommonSubstitutionBase = ' ';
+        if (!votes.substitutionBases.empty()) {
+            map<char, int> substitutionBaseCounts;
+            for (char base : votes.substitutionBases) {
+                substitutionBaseCounts[base]++;
+            }
+            mostCommonSubstitutionBase = max_element(substitutionBaseCounts.begin(), substitutionBaseCounts.end(),
+                                                     [](const pair<char, int>& a, const pair<char, int>& b) {
+                                                         return a.second < b.second;
+                                                     })->first;
+        }
+
+        string mostCommonInsertionBase;
+        if (!votes.insertionBases.empty()) {
+            map<string, int> insertionBaseCounts;
+            for (const string& bases : votes.insertionBases) {
+                insertionBaseCounts[bases]++;
+            }
+            mostCommonInsertionBase = max_element(insertionBaseCounts.begin(), insertionBaseCounts.end(),
+                                                   [](const pair<string, int>& a, const pair<string, int>& b) {
+                                                       return a.second < b.second;
+                                                   })->first;
+        }
+
+        string mostCommonDeletionBase;
+        if (!votes.deletionBases.empty()) {
+            map<string, int> deletionBaseCounts;
+            for (const string& bases : votes.deletionBases) {
+                deletionBaseCounts[bases]++;
+            }
+            mostCommonDeletionBase = max_element(deletionBaseCounts.begin(), deletionBaseCounts.end(),
+                                                 [](const pair<string, int>& a, const pair<string, int>& b) {
+                                                     return a.second < b.second;
+                                                 })->first;
+        }
+
+        // Odabir najčešćeg znaka za svaku vrstu mutacije
+        char base;
+        if (votes.substitutionVotes > votes.insertionVotes && votes.substitutionVotes > votes.deletionVotes) {
+            base = mostCommonSubstitutionBase;
+        } else if (votes.insertionVotes > votes.substitutionVotes && votes.insertionVotes > votes.deletionVotes) {
+            base = mostCommonInsertionBase.empty() ? '-' : mostCommonInsertionBase[0];
+        } else {
+            base = mostCommonDeletionBase.empty() ? '-' : mostCommonDeletionBase[0];
+        }
+
+        // Ispis rezultata
+        if (base != ' ') {
+            if (votes.substitutionVotes >= votes.insertionVotes && votes.substitutionVotes >= votes.deletionVotes) {
+                outfile << "substitution,X," << pos << "," << base << "\n";
+            } else if (votes.insertionVotes >= votes.substitutionVotes && votes.insertionVotes >= votes.deletionVotes) {
+                outfile << "insertion,I," << pos << "," << base << "\n";
+            } else {
+                outfile << "deletion,D," << pos << "," << base << "\n";
+            }
         }
     }
 
@@ -185,3 +250,4 @@ int main() {
     cout << "Mutation detection completed, output saved to " << outputCsv << endl;
     return 0;
 }
+
